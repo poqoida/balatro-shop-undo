bdc1e06dc91b17c9b2e2a7daaa29e81e83e27f88
swagdoe <poqoida@icloud.com>
2026-06-25 08:44:32 +0300

    Delete balatro-shop-undo directory

--- balatro-shop-undo/ShopUndo/config.lua
+++
@@ -1,9 +0,0 @@
-return {
-    undo_price            = 0,
-    max_undo              = 5,
-    refund_reroll         = true,
-    sell_back_purchases   = true,
-    burn_used_consumables = true,
-    undo_on_right_click   = true,
-    undo_key              = 'u',
-}
