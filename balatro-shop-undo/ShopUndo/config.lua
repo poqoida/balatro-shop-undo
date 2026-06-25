1a720a9f515927cdbc4463591ebfbbacbbf3b90c
swagdoe <poqoida@icloud.com>
2026-06-25 08:46:59 +0300

    Add files via upload

---
+++ balatro-shop-undo/ShopUndo/config.lua
@@ -0,0 +1,9 @@
+return {
+    undo_price            = 0,
+    max_undo              = 5,
+    refund_reroll         = true,
+    sell_back_purchases   = true,
+    burn_used_consumables = true,
+    undo_on_right_click   = true,
+    undo_key              = 'u',
+}
